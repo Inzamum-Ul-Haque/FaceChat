b3cb05225af051d41861f93d3eb75e0f9aedd8f8
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-12-26 23:31:17 +0600

    message form and message ui structure

--- src/Components/MessageForm/MessageForm.js
+++ src/Components/MessageForm/MessageForm.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { isTyping, sendMessage } from "react-chat-engine";
 
 const MessageForm = () => {
-  return <div>Message form</div>;
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {};
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="message-form">
+      <input
+        onSubmit={handleSubmit}
+        type="text"
+        value={value}
+        placeholder="Send a message ... "
+        onChange={handleChange}
+        className="message-input"
+      />
+    </form>
+  );
 };
 
 export default MessageForm;
